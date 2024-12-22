828333d33fa10130574f3714cd1c191bcd690110
Batiman00 <lucas.alex2000@usp.br>
2024-12-22 15:03:59 -0300

    CORS config

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
-    methods: 'GET,POST', 
+    origin: process.env.NEXT_URL,
+    methods: 'GET,POST,OPTION', 
     allowedHeaders: 'Content-Type, Authorization, X-CSRF-Token',
     credentials: true,
   });
