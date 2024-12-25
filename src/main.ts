b87ac544f2150bf4d75ad43aa3f31cda65f602f4
Batiman00 <lucas.alex2000@usp.br>
2024-12-25 18:57:42 -0300

    Enable CORS *

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: process.env.NEXT_URL,
+    origin: '*',
     methods: 'GET,POST,OPTIONS', 
     allowedHeaders: 'Content-Type, Authorization, X-CSRF-Token',
     credentials: true,
